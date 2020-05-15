bc99f66659237e8eeb252e1c43ee7117a613c452
Said <s.lotus69@gmail.com>
2020-05-15 16:26:29 +0200

    fix for review

--- starter_code/backend/src/auth/auth.py
+++ starter_code/backend/src/auth/auth.py
@@ -91,7 +91,7 @@ def check_permissions(permission, payload):
         raise AuthError({
             'code': 'unauthorized',
             'description': 'Permission not found.'
-        }, 403)
+        }, 401)
     return True
 
 
